a63db957092d724e897f8576d0ac8fb65750a0d0
Corinne <corinne@Zipcoders-MacBook-Pro.local>
2023-10-27 08:59:48 -0400

    First attempt, needs work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,42 @@
 
 import java.util.Scanner;
 import java.util.logging.Logger;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args){
+
+        // Create logger
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("Hey, is this working?");
+
+        // create instance of Random class
+        Random rand = new Random();
+
+        //Get random number between 1 and 10
+        int target = rand.nextInt(10)+1;
+
+        //Check what the number is
+        logger.info(String.valueOf(target));
+
+        // Create scanner
+        Scanner scanner = new Scanner(System.in);
+        // Get user input
+        System.out.print("Guess a number between 1 and 10: ");
+        int guess = scanner.nextInt();
+
+        do {
+            if (guess > target) {
+                System.out.print("Too high, enter a new guess: ");
+                guess = scanner.nextInt();
+            } else {
+                System.out.print("Too low, enter a new guess: ");
+                guess = scanner.nextInt();
+            }
+        } while (guess != target);
+
+        System.out.print("You got it, the answer is " + target + "!");
+
+
     }
 }
