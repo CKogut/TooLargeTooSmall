1998ead573b8f04bfbe276cf3bf5dcde0018581c
Corinne <corinne@Zipcoders-MacBook-Pro.local>
2023-10-27 08:11:44 -0400

    Getting started

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-        
+        Logger logger=Logger.getLogger(Main.class.getName());
+        logger.info("Hey, is this working?");
     }
 }
